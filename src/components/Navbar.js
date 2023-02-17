54dda01f027b81179ee2d65fa4e36f5d3e2e8c1d
dangtung <tungdang1997@example.com>
2023-02-17 10:06:06 +0700

    them add, list, logout, home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export default function Navbar() {
     return (
         <>
@@ -5,7 +7,7 @@ export default function Navbar() {
                 <div className='col-12'>
                     <nav className="navbar navbar-expand-lg bg-body-tertiary">
                         <div className="container-fluid">
-                            <a className="navbar-brand" href="#">Navbar</a>
+                            <Link className="navbar-brand" to="/home">LoL</Link>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                     aria-expanded="false" aria-label="Toggle navigation">
@@ -14,7 +16,7 @@ export default function Navbar() {
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                        <Link className="nav-link active" aria-current="page" to="/add-blog">Add Blog</Link>
                                     </li>
                                     <li className="nav-item">
                                         <a className="nav-link" href="#">Link</a>
@@ -37,6 +39,11 @@ export default function Navbar() {
                                         <a className="nav-link disabled">Disabled</a>
                                     </li>
                                 </ul>
+                                <form className="d-flex" role="search">
+                                    <Link to={'/'}>
+                                        <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Logout</button>
+                                    </Link>
+                                </form>
                             </div>
                         </div>
                     </nav>
